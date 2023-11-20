7e83cf61762ad88315a918149e3894f4b1f3a488
alexisvannson <147932138+alexisvannson@users.noreply.github.com>
2023-11-21 00:22:51 +0100

    Create grocery list.py

---
+++ grocery list.py
@@ -0,0 +1,15 @@
+food = []
+food_dict = { }
+while True:
+    try:
+       food.append(input("").upper())
+       if food[-1] in food_dict:
+            food_dict[food[-1]] += 1
+       else:
+           food_dict[food[-1]] = 1
+    except EOFError:
+        sorted_dict = {key: food_dict[key] for key in sorted(food_dict)}
+        for keys,values in sorted_dict.items():
+            print(values,keys)
+
+        break
